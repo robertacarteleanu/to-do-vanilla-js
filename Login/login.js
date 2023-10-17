6548d0f7b6bdeb51749cacaafd11753a993a5bc5
unknown <roberta.carteleanu@yahoo.com>
2023-10-17 19:39:15 +0300

    draft

--- Login/login.js
+++ Login/login.js
@@ -1,4 +1,30 @@
+import{users} from '../Database/users.js'
+
 export function login(e){
-    console.log("vrei sa te autentifici")
     e.preventDefault()
+    const loginEmail= document.getElementById("loginEmail")
+    const loginPassword= document.getElementById("loginPassword")
+    console.log(`vrei sa te autentifici cu ${loginEmail.value} si ${loginPassword.value}`)
+
+    const user=users.filter(function(user){
+        return user.email===loginEmail.value;
+    });
+    if(user[0]){
+        console.log('utilizator gasit: ', user[0]);
+        if(user[0].password===loginPassword.value){
+            localStorage.setItem('isLoggedIn',true);
+            localStorage.setItem('username',user[0].name);
+            document.getElementById("login").setAttribute('hidden', 'true')
+            document.getElementById("register").setAttribute('hidden', 'true')
+            document.getElementById("user").removeAttribute('hidden')
+            document.getElementById("greetings").textContent='Salut '+user[0].name
+
+        }
+
+    }
+    else{
+        console.log(`utilizator nu exista`);
+
+    }
 }
+
